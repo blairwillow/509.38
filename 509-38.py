c487e851a62577b5ed0681488edacbb193f227ed
blairwillow <pan20072550@gmail.com>
2024-12-02 14:27:48 +0700

    Add files via upload

---
+++ 509-38.py
@@ -0,0 +1,11 @@
+province = ["กรุงเทพมหานคร","กาฬสินธ์ุ","กาญจนบุรี","จันทบุรี","ฉะเชิงเทรา"
+            ,"ชลบุรี","พะเยา","ชุมพร","ชัยนาท","ขอนแก่น","ชัยภูมิ","ยโสธร"
+            ,"นครพนม","นครราชสีมา","เชียงใหม่"]
+
+province[0] = "Bangkok"
+province[13] = "Korat"
+province[4] = "Peat Riw"
+province[5] = "Chon buri"
+province[7] = "Chum porn"
+
+print(province)
